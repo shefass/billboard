bd200a44e7873d3d2aca7ba97916f9a77fff5c35
Shefass <shefass@gmail.com>
2019-06-20 23:04:57 +0300

    Updates

---
+++ precache-manifest.ecf9d58e41b1db14ecca4832e7f770d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ce6ef40fc7498b477677fd348c32b8",
+    "url": "/billboard/index.html"
+  },
+  {
+    "revision": "de63fbbcef2a07a49e88",
+    "url": "/billboard/static/css/main.2b513c7b.chunk.css"
+  },
+  {
+    "revision": "cbb1888d132a4b4e82a7",
+    "url": "/billboard/static/js/2.e371ef49.chunk.js"
+  },
+  {
+    "revision": "de63fbbcef2a07a49e88",
+    "url": "/billboard/static/js/main.4bc6efe4.chunk.js"
+  },
+  {
+    "revision": "8281d74e26e4ee537276",
+    "url": "/billboard/static/js/runtime~main.7c512d5a.js"
+  },
+  {
+    "revision": "31d043a02660ae5138fa03fbbc67b26b",
+    "url": "/billboard/static/media/burstjs.31d043a0.png"
+  }
+]);
